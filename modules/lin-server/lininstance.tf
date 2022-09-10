fbec57c7a97d7dc259a1b92ef83b9a592deea30d
Gourab Nandi <gngourabnandi@gmail.com>
2022-09-10 05:58:23 +0000

    Updated Execute

--- modules/lin-server/lininstance.tf
+++ modules/lin-server/lininstance.tf
@@ -18,10 +18,9 @@ resource "aws_instance" "lin-EC2" {
   }
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
+      "sudo yum install git -y",
       "sudo yum install httpd -y",
       "sudo systemctl start httpd",
-      "sudo yum install git -y",
       "sudo yum install java -y",
       "sudo yum install java-devel -y",
       "export JAVA_HOME=(/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.322.b06-2.el8_5.x86_64/jre)",
