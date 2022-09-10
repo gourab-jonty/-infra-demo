a55aaf0247294a6790e46c280b9aeab525363475
Gourab Nandi <gngourabnandi@gmail.com>
2022-09-10 07:47:05 +0000

    Updated User-Data-1

--- modules/lin-server/lininstance.tf
+++ modules/lin-server/lininstance.tf
@@ -19,6 +19,11 @@ resource "aws_instance" "lin-EC2" {
       sudo yum install git -y
       sudo yum install httpd -y
       sudo systemctl start httpd
+      sudo yum install java -y
+      sudo yum install java-devel -y
+      export JAVA_HOME=(/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.322.b06-2.el8_5.x86_64/jre)
+      sudo yum install openssh-server
+      mkdir ~/jenkins
       EOF
   
   /*provisioner "remote-exec" {
@@ -29,7 +34,7 @@ resource "aws_instance" "lin-EC2" {
     ]
   }*/
 
-  provisioner "remote-exec" {
+  /*provisioner "remote-exec" {
     inline = [
       "sudo yum install java -y",
       "sudo yum install java-devel -y",
@@ -46,6 +51,6 @@ resource "aws_instance" "lin-EC2" {
     user        = "ec2-user"
     host        = self.private_ip
     private_key = file("${path.module}/key/ec2.pem")
-  }
+  }*/
 
 }
