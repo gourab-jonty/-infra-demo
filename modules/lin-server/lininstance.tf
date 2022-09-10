adacab3352cf9faa25319a2f0e268a12fe62fefa
Gourab Nandi <gngourabnandi@gmail.com>
2022-09-10 18:43:28 +0000

    Updated Scripts 1

--- modules/lin-server/lininstance.tf
+++ modules/lin-server/lininstance.tf
@@ -21,15 +21,14 @@ resource "aws_instance" "lin-EC2" {
   }*/
   provisioner "remote-exec" {
     inline = [
-      "sudo su",
-      "yum update -y",
-      "yum install git -y",
-      "yum install httpd -y",
-      "yum install java -y",
-      "yum install java-devel -y",
+      "sudo yum update -y",
+      "sudo yum install git -y",
+      "sudo yum install httpd -y",
+      "sudo yum install java -y",
+      "sudo yum install java-devel -y",
       "export JAVA_HOME=(/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.322.b06-2.el8_5.x86_64/jre)",
       "mkdir ~/jenkins",
-      "yum install openssh-server -y",
+      "sudo yum install openssh-server -y",
       "systemctl start httpd"
     ]
   }
